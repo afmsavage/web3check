dc35ec92f119218cb81219ee72eaddd1429929da
Antonio Savage <afmsavage@gmail.com>
2020-10-19 06:50:28 -0500

    added css and eslint

--- src/script.js
+++ src/script.js
@@ -1,42 +1,44 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
 const showTdts = document.getElementById('tdtArea');
-//const axios = require('axios').default;
+const showBond = document.getElementById('bond');
+const axios = require('axios').default;
+import './style.css';
 
 ethereumButton.addEventListener('click', () => {
   getAccount();
 });
 
-// async function getAccount() {
-//   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-//   const account = accounts[0];
-//   showAccount.innerHTML = account;
-//   getTdts(account);
-// }
-
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
   showAccount.innerHTML = account;
-  let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${account}`
-  fetch(url)
-    .then(response => response.json())
-    .then(function (data) {
-      let allTdts = []
-      data.forEach(element => allTdts.push(element));
-      let aString = allTdts.toString();
-      showTdts.innerText = aString;
-    })
+  getBond(account);
+  getTdts(account);
 }
 
-// async function getTdts(addr) {
-//   let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${addr}`
-//   try {
-//     await axios.get(url).then(function (response) {
-//       showTdts.innerText(response.data)
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
+async function getTdts(addr) {
+  let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${addr}`
+  try {
+    await axios.get(url)
+      .then(function (response) {
+        let allTdts = []
+        response.data.forEach(element => allTdts.push(element))
+        showTdts.innerText = allTdts.toString();
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+async function getBond(addr) {
+  let url = `https://backendfortbtc.azurewebsites.net/api/getBond?wallet=${addr}`
+  try {
+    await axios.get(url)
+      .then(function (response) {
+        showBond.innerText = response.data
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/script.js',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
 };
