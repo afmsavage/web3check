e6f397d2aaf1d4a660749edb70fe81cc98e955d5
Antonio Savage <afmsavage@gmail.com>
2020-10-18 22:13:50 -0500

    commiting currently since its working

--- src/script.js
+++ src/script.js
@@ -1,26 +1,42 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
-const showTdts = document.querySelector('.showTdts');
-const axios = require('axios').default;
+const showTdts = document.getElementById('tdtArea');
+//const axios = require('axios').default;
 
 ethereumButton.addEventListener('click', () => {
   getAccount();
 });
 
+// async function getAccount() {
+//   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+//   const account = accounts[0];
+//   showAccount.innerHTML = account;
+//   getTdts(account);
+// }
+
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
   showAccount.innerHTML = account;
-  getTdts(account);
+  let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${account}`
+  fetch(url)
+    .then(response => response.json())
+    .then(function (data) {
+      let allTdts = []
+      data.forEach(element => allTdts.push(element));
+      let aString = allTdts.toString();
+      showTdts.innerText = aString;
+    })
 }
 
-async function getTdts(addr) {
-  let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${addr}`
-  try {
-    await axios.get(url).then(function (response) {
-      showTdts.appendChild(response.data)
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function getTdts(addr) {
+//   let url = `https://backendfortbtc.azurewebsites.net/api/getTdts?wallet=${addr}`
+//   try {
+//     await axios.get(url).then(function (response) {
+//       showTdts.innerText(response.data)
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
